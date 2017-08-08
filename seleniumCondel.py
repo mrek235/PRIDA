b881f740a9c4518bd9bb24bf97dce44fb3e5606f
mrek235 <30494993+mrek235@users.noreply.github.com>
2017-08-08 03:07:04 +0300

    Add files via upload
    Sonradan gelenler

--- seleniumCondel.py
+++ seleniumCondel.py
@@ -1,14 +1,28 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException as ERROR
+from selenium.webdriver.common.keys import Keys
+
 
 browser = webdriver.Firefox()
 browser.get("http://bg.upf.edu/fannsdb/signin?next=%2Ffannsdb%2Fquery%2Fcondel")
-login = browser.find_element_by_class("btn persona-button")
+login = browser.find_element_by_id("username")
 login.click()
-email  = browser.find_element_by_name("email")
-email.send_keys("ogulcan.cingiler@ozu.edu.tr")
-password   = browser.find_element_by_name("password")
-submit = browser.find_element_by_class_name("auth0-lock-submit")
+
+for i in range (0,5):
+	login.send_keys(Keys.TAB)
+
+login.send_keys("pridaproject@gmail.com")
+#email  = browser.find_element_by_xpath("//input[@name='email']")
+login.send_keys(Keys.TAB)
+#password   = browser.find_element_by_name("password")
+login.send_keys("sirdancealot")
+login.send_keys(Keys.TAB)
+login.click()
+#submit = browser.find_element_by_class_name("auth0-lock-submit")
+#submit.click()
+	
+
 
 mutation_area = browser.find_element_by_link_text("load mutations" )
 mutation_area.click()
@@ -17,7 +31,8 @@ mutation_area.send_keys("/home/mrek/helomalatya.vcf")
 name = browser.find_element_by_id("project_name")
 name.send_keys("PRIDA")
 
-
+query = browser.find_element_by_type("submit")
+query.c1ick()
 
 
 
